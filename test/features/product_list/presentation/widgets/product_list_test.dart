5885be76a9129d53325648b3158a425e5631f134
Sayeed <sayeedhussain19@gmail.com>
2019-12-23 23:38:33 +0530

    [Sayeed] Add more tests to products_list_test

--- test/features/product_list/presentation/widgets/product_list_test.dart
+++ test/features/product_list/presentation/widgets/product_list_test.dart
@@ -13,12 +13,12 @@ void main() {
       '[{\"upc\":\"0003\",\"name\":\"Apple iPhone 7\",\"price\":\"\$1,099.99\",\"commentsCount\":2,\"image\":\"https:\/\/m.media-amazon.com\/images\/I\/31UU-oejIwL._AC_UY400_QL65_ML3_.jpg\"},{\"upc\":\"0004\",\"name\":\"Simple Mobile Prepaid\",\"price\":\"\$169.45\",\"commentsCount\":7,\"image\":\"https:\/\/m.media-amazon.com\/images\/I\/61ty1FGnI4L._AC_UY436_QL65_ML3_.jpg\"}]'));
   testWidgets('Load Products', (WidgetTester tester) async {
     provideMockedNetworkImages(() async {
-      /// Now we can pump NetworkImages without crashing our tests. Yay!
       await tester.pumpWidget(MaterialApp(
           home: Scaffold(body: ProductsList(products: _getProducts()))));
-
-    //    Expect to find the item on screen.
-    expect(find.text('Apple iPhone 7'), findsOneWidget);
+      expect(find.text('Apple iPhone 7'), findsOneWidget);
+      expect(find.text('\$1,099.99'), findsOneWidget);
+      expect(find.text('Simple Mobile Prepaid'), findsOneWidget);
+      expect(find.text('\$169.45'), findsOneWidget);
     });
   });
 }
