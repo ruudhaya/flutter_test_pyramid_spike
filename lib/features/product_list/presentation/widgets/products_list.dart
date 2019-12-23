461cc746b008fba3c22dd3e0d1db8f290cd68347
Sayeed <sayeedhussain19@gmail.com>
2019-12-23 22:07:30 +0530

    [Sayeed] Remove unused import

--- lib/features/product_list/presentation/widgets/products_list.dart
+++ lib/features/product_list/presentation/widgets/products_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_pyramid_spike/features/product_list/domain/entities/product.dart';
 import 'package:flutter_test_pyramid_spike/features/product_list/presentation/widgets/product_list_item.dart';
-import 'package:flutter_test_pyramid_spike/features/product_list/presentation/widgets/product_list_item_imageview.dart';
 
 class ProductsList extends StatelessWidget {
   ProductsList({@required this.products});
